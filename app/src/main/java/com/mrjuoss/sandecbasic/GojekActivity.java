80b8fae0f0a108eecc52cffab8bdcb9e32ff3001
mrjuoss <mrjuoss@gmail.com>
2019-03-10 13:09:35 +0700

    Add activity and layout like Simple Gojek App

---
+++ app/src/main/java/com/mrjuoss/sandecbasic/GojekActivity.java
@@ -0,0 +1,13 @@
+package com.mrjuoss.sandecbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GojekActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gojek);
+    }
+}
