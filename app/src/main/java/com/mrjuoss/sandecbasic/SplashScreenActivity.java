a90d220eaeeaa64d181da7c22d9ecd2f386196f4
mrjuoss <mrjuoss@gmail.com>
2019-03-10 19:32:44 +0700

    Add simple splash screen

---
+++ app/src/main/java/com/mrjuoss/sandecbasic/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.mrjuoss.sandecbasic;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, GojekActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
