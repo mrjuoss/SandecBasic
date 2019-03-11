a6fcc5275b12931f21caa4cb7817f7e765cdcc26
mrjuoss <mrjuoss@gmail.com>
2019-03-11 14:59:31 +0700

    Add simple sharedpreference login

---
+++ app/src/main/java/com/mrjuoss/sandecbasic/LoginSharedPreferenceActivity.java
@@ -0,0 +1,13 @@
+package com.mrjuoss.sandecbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSharedPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_shared_preference);
+    }
+}
